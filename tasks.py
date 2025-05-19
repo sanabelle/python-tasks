8b62d8404cc8156218e86aa0edcc6dab82c47bfb
sanabelle <112629085+sanabelle@users.noreply.github.com>
2025-05-19 06:38:42 +0300

    Add files via upload

---
+++ tasks.py
@@ -0,0 +1,21 @@
+# Write a Python program that calculates the area of a circle based on the radius entered by the user.
+Redias=int(input("enter the Redias of the Circle:..."))
+print("the area of the circle is"," ",3.14*(Redias**2))
+
+# Write a Python program that accepts the user's first and last name and prints them in reverse order with a space between them.
+f_name=str(input("Enter your first name.."))
+l_name=str(input("Enter your last name.."))
+print("your verse name is...",l_name,f_name)
+
+# Write a Python program that accepts an integer (n) and computes the value of n+nn+nnn
+n=int(input("Enter a number: "))
+print("the value of n+nn+nnn is...",n+(n**2)+n**3)
+
+
+# Write a Python program to display the examination schedule. (extract the date from exam_st_date).
+exam_st_day =int(input("enter exam day:...."))
+exam_st_month =int(input("enter exam month:...."))
+exam_st_year =int(input("enter exam year:...."))
+print("the examination schedule is...",exam_st_day,"/",exam_st_month,"/",exam_st_year)
+
+
