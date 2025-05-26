554dd28314c36f4a4db52a60f34326c1d4f033c7
sanabel <ssssasomostafa123@gmail.com>
2025-05-26 08:38:21 +0300

    day6 tasks

--- tasks.py
+++ tasks.py
@@ -7,7 +7,52 @@
 
 
 
-#sanabel
+
+
+# student_mark=int(input("Enter your mark"))
+
+# if(student_mark >=85):
+#     print("A")
+# elif(student_mark>=75 and student_mark<85 ) :
+#     print("B")
+# elif(student_mark>=65 and student_mark <75):
+#     print("C")
+# else:
+#     print("fail")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# data={
+#     'ahmed' : {'name' :'ahmed', 'age' : 27, 
+#                'phone' : {'num1': [12587,36985,8], 
+#                           'num2' : [1,8,933,[89,{'ed' : 1478, 
+#                                                  "ed2" : [78569,9631]}]]}}
+# }
+# user_phone=int(input ("enter your phone:..."))
+# data["ahmed"]['phone']['num2'][3][1]['ed2'][1]=user_phone
+# print(data)
+
+
+
+# data={
+# 'name':'sanabel',
+# 'phone':1021452,
+# }
+# data["name"]='soso'
+# print(data['name'])
+
+
 
 
 
@@ -142,3 +187,189 @@ print(f'10*{usernumber}={10*usernumber}')
 
 
 
+
+
+
+
+
+
+#Day_4 TASKS//////////////////////////////( Products System)
+#4))
+# Write a supermarket management program.
+# There is a database containing the following: the name of each product, 
+# the quantity available in the warehouse, whether it is on sale, 
+# the product's price, and the product's section.
+# The program displays the product name, the price per item, 
+# and a brief description. It displays all products in a simple and attractive way, 
+# with each product having its own unique number.
+# Then, the user is prompted to first select the product numbers they want to purchase. 
+# After entering the numbers, a request appears, 
+# specifying the required quantities for each product.
+# Then, the purchase invoice displays the product name, 
+# the price per item, the price per selected item, the quantity for each product, 
+# and the total amount due. A discount is added to the product, 
+# if applicable, and this is noted on the invoice. Example:
+#
+# ===============Welcome===============
+# product 1 :
+# Name -> milk
+# Price -> 10$
+# Description -> Fresh and healthy milk
+# -----
+# product 2 :
+# Name -> Rice
+# Price -> 20$
+# Description -> Premium and pure rice
+# -----
+# product 3 :
+# Name -> Bread
+# Price -> 2$
+# Description -> fresh bread
+# =====================================
+# Please write the numbers of the products you want to buy like this 1,2 : 1,3
+# Please write the value of each product you want to purchase in order of your previous selection : 2,5
+#
+# ================Purchase Invoice================
+# Product Name        Price per piece        Number of pieces        Total price
+#
+# milk                10$                    2                       20$
+# Bread               2$                     5                       10$
+#
+# Total due before discount : 30$
+# Total due after discount  : 25$
+
+products={
+   'product1':{
+   'name':"milk",
+   'price':10,
+   'description':'Fresh and healthy milk',
+   'quantity': 40,
+   'on_sale' : False
+},
+
+   'product2':{
+   'name':"Rice",
+   'price':50,
+   'description':'Premium and pure rice',
+   'quantity': 60,
+   'on_sale' : True
+},
+
+
+'product3':{
+   'name':"Bread",
+   'price':10,
+   'description':'Fresh bread',
+   'quantity': 100,
+   'on_sale' : False
+}
+
+}
+
+x=list(products.keys())
+print('=================Welcome===================')
+check=0
+while check<len(x):
+ print(f'{x[check]}:')
+ print(f'Name<={products[x[check]]['name']}')
+ print(f'Price<={products[x[check]]['price']}')
+
+ print(f'Description<={products[x[check]]['description']}')
+ print('!!!!!!!!!!!!!!!!!')
+ check+=1
+print('========================================')
+
+
+
+products_number=input("enter products number you want to order like this 1 2: ")
+products_quantity=input("enter products quantities you want to order like this 6 5: ")
+print('========================================')
+
+x2=products_number.split(' ')
+x3=products_quantity.split(' ')
+x_mix=dict(zip(x2,x3))
+x_mix_keys=list(x_mix.keys())
+x_mix_values=list(x_mix.values())
+
+check3=0
+# print(x[z])
+while check3<len(x_mix_keys):
+   y=x_mix_keys[check3]#2
+   y2=x_mix_values[check3]
+   z=int(y)-1#2-1=1
+   print(f'you have ordered {products[x[z]]['name']} for quantites: {int(y2)}')
+   
+
+
+  
+   check3+=1
+   # total2=list(total)
+   # print(total2)
+print('========================================')
+
+Purchase_Invoice=['Product Name','Price per piece','Number of pieces','Total price']#دي كل مرة بتتكرر
+print(Purchase_Invoice)
+
+check4=0
+while check4<len(x_mix_keys):
+   y=x_mix_keys[check4]#2
+   y2=x_mix_values[check4]
+   z=int(y)-1#2-1=1
+
+   price=products[x[z]]['price']
+# print(price)
+   total=price*int(y2)
+   Purchase_Invoice2=[products[x[z]]['name'],products[x[z]]['price'],int(y2),total]
+   print(Purchase_Invoice2)
+   check4+=1
+print('========================================')
+
+
+
+
+
+
+
+
+
+#Day_6 TASKS//////////////////////////////
+#1)) Print only even numbers from 1 to n
+n=input("enter a number: ")
+for i in range(1,int(n),2):
+    print(i)
+
+#2))Write a function that adds all the numbers in a list.
+numbers=[5,10,12,14,20]
+total=0
+for i in range(0,len(numbers)):
+    total+=numbers[i]
+
+print(total)
+
+
+#3)) text = 'hello my name is abdallah' -> 4
+
+# number of specific letters in a text
+user_text=input("enter A text: ").lower() #any string is a list of its value (hello sanabel)<<=[h,e,l,l,o,s,a,n,a,b,e,l]
+specifi_letter='a'
+total=0
+for i in user_text:
+    if i==specifi_letter:
+        total+=1
+print(total)
+
+#4)) Print triangle stars
+#        *
+#        **
+#        ***
+#        ****
+#        *****
+#        ******
+#        *******
+#        ********
+#        **********
+#        ***********
+stars=['*',['*,*'],['*,*,*'],['*,*,*,*'],['*,*,*,*,*'],['*,*,*,*,*,*'],['*,*,*,*,*,*,*'],['*,*,*,*,*,*,*,*'],['*,*,*,*,*,*,*,*,*'],['*,*,*,*,*,*,*,*,*,*']]
+for i in stars:
+    for u in i:
+        print(u)
